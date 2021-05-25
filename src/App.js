70cb8e8df2cefb8c177490adc8ae6f8df177c513
veys <veysel.bult@gmail.com>
2021-05-25 18:02:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import React from 'react';
+import Reviews from './Reviews';
+
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <Reviews/>
+    </div>
+  );
+}
